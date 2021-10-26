4d18d393399c4ad47b480f1cc04880e87379989f
Andrii <achuliy99@gmal.com>
2021-10-26 16:55:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// "use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// let persone = 'akex';
+// const me = {
+//     name:'Andrew',
+//     age: 22,
+//     isMarried: false
+// }
+
+// console.log(me.name);
+
+// let arr = ['ebm', 4, 'techno'];
+// console.log(arr[1]);
+
+//alert("zdarova");
+// const result = confirm("are you here?");
+// console.log(result);
+// const answer = prompt("Do you have a beer?", "");
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+//const category = 'toys';
+
+//console.log(`https://url.com/${category}/${5}/${category}`);
+
+const bro = 'Andryha';
+alert(`zdarova, ${bro}`);
